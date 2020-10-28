ce6b53b15e5011fb7e6e083d97b4ecc7467eace8
Jesus Israel Anaya Salazar <anayaj0511@hotmail.com>
2020-10-27 20:32:32 -0600

    add user sample

--- build.bat
+++ build.bat
@@ -3,7 +3,10 @@ echo -----------------------------------------------------------------
 echo Samples Startup Framework
 echo -----------------------------------------------------------------
 
+call mvn -f sample-domain-user/pom.xml clean install
 call mvn -f sample-domain-customer/pom.xml clean install
+
+call mvn -f sample-ms-et-user/pom.xml clean package
 call mvn -f sample-ms-et-customer/pom.xml clean package
 call mvn -f sample-ms-mt-customer-identity/pom.xml clean package
 call mvn -f sample-ms-ts-customer/pom.xml clean package
--- clean.bat
+++ clean.bat
@@ -3,8 +3,10 @@ echo -----------------------------------------------------------------
 echo Samples Startup Framework
 echo -----------------------------------------------------------------
 
-
+call mvn -f sample-domain-user/pom.xml clean
 call mvn -f sample-domain-customer/pom.xml clean
+
+call mvn -f sample-ms-et-user/pom.xml clean
 call mvn -f sample-ms-et-customer/pom.xml clean
 call mvn -f sample-ms-mt-customer-identity/pom.xml clean
 call mvn -f sample-ms-ts-customer/pom.xml clean
